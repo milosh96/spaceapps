8939e605382b1a3480bbed63fae4063f3c9957c8
Nikola Pavlović <nikola825@gmail.com>
2018-10-20 17:13:38 +0200

    sdjhfksdf

--- SpaceApp/SpaceApp/Content/apiGet.js
+++ SpaceApp/SpaceApp/Content/apiGet.js
@@ -1,6 +1,6 @@
 ﻿function getLaunchesForDateRange(from, to, callback)
 {
-    var launchUrl = `https://launchlibrary.net/1.4/launch/${from}/${to}?limit=50`;
+    var launchUrl = `https://launchlibrary.net/1.4/launch/${from}/${to}?limit=100`;
     $.ajax(launchUrl, {
         method: "GET",
         success: function (result)
